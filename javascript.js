ceae66005526db206dacb91f4a362eeb73db1aea
CodBen <junes828@gmail.com>
2021-02-12 07:34:29 +0100

    jquery slide section

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-console.log('mon js est chargé');
+
+//////////////////////////////////////////////////////////////
+/////////////// JQUERY SELECT AND HIDE ELEMENTS /////////////
+
+$('#form').hide();                          // Sélecteur du formulaire
+$('#discover').click(() => {               // Sélecteur du bouton discover
+    $('#form').slideToggle();              // Déclenchement de l'évent -> fait apparaitre le formulaire
+});
